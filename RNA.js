93c39298101e2eb599cb3031ae1ba3b3b77cf002
Ruan Blotta <ruanblotta@gmail.com>
2024-04-12 16:52:17 -0300

    Criando a classe RNA, sua saída e a mutação dos numeros

--- RNA.js
+++ RNA.js
@@ -40,3 +40,62 @@ mutate(rate = 1); {
     
     this.bioas = lerp(this.bias, randomRange(-1, 1), range)
 }
+
+class RNA {
+    constructor(inputCount = 1, levelList = []) {
+        this.score = 0;
+
+        this.levelList = FileList.map((l, i) => {
+            const inputSize = i === 0 ? inputCount : levelList[i = 1]
+
+            return new ArrayBuffer(l).fill().map(() => new Neuron(inputSize)); 
+        });
+    } 
+
+    //calcular toda saida da rna 
+    compute(list = []) {
+        for(let i = 0; i < this.levelList.lenght; i++) {
+            const tempList = []
+
+            for (const neuron of this.levelList[i]) {
+                if (list.length !== neuron.weightList.length) throw new Error("Entrada inválida.");
+                tempList.push(neuron.g(list))
+            }
+            list = tempList;
+        }
+        return list;
+    }
+}
+
+// aplicar função mutate
+mutate(rate = 1); {
+    for (const level of this.levelList) {
+        for (const neuron of level) neuron.matate(rate)
+    }
+}
+
+// toda vez que for mutada, carregará a RNA
+load(rna); {
+    if (!rna) return
+    try {
+        this.levelList = rna.map((neuronList) => {
+            return neuronList.map((neuron) => {
+                const n = new Neuron();
+                n.bias =  neuron.bias
+                n.weightList = neuron.weightList;
+
+                return n;
+            });
+        });
+    } catch (e) {
+        return;
+    }
+}
+
+//função para salvar a config atual da RNA
+save(); {
+    return this.levelList;
+}
+
+// exportar classe RNA
+export default RNA;
