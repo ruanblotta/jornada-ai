30748190b5540dc8da6fb766002a1a784263a43d
Ruan Blotta <ruanblotta@gmail.com>
2024-04-18 22:02:12 -0300

    Implementando Agachar no controls.js

--- controls.js
+++ controls.js
@@ -1,5 +1,6 @@
 export default {
     jump: new KeyboardEvent('keydown', {key: 'space', keyCode: 32}),
+    crouch: new KeyboardEvent('keydown', {key: 'ArrowDown', keyCode: 40}),
     dispatchEvent(event) {
         document.dispatchEvent(this[event]);
     }
